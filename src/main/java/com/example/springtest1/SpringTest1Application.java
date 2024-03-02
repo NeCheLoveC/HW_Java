dd5968fb3cd050f0ac924145f1e40dc01bc17ae6
Anton Protsenko <protseng@mail.ru>
2024-03-02 18:41:42 +0400

    init

---
+++ src/main/java/com/example/springtest1/SpringTest1Application.java
@@ -0,0 +1,14 @@
+package com.example.springtest1;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringTest1Application {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(SpringTest1Application.class, args);
+    }
+}
